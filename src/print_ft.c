a55a4f203ce33b3306ab3c252c2ebb95e2de699b
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-03-11 16:51:30 +0100

    avancement sur les fonction de printage

--- src/print_ft.c
+++ src/print_ft.c
@@ -5,8 +5,15 @@
 ** All print function for strace
 */
 
-#include <stdio.h>
+#include <sys/ptrace.h>
+#include <sys/reg.h>
+#include <sys/signal.h>
+#include <sys/user.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include "my.h"
+#include "strace.h"
 
 int print_error(char *str)
 {
@@ -22,4 +29,48 @@ int exit_strace(int value_status, size_t option)
         return 0;
     }
     return 84;
+}
+
+static s_syscall_t *get_syscall_by_value(int id)
+{
+    int i = 0;
+
+    while (g_syscalls[i].name) {
+        if (g_syscalls[i].id == id)
+            return (&g_syscalls[i]);
+        i++;
+    }
+    return NULL;
+}
+
+static void print_arguement_function(struct user_regs_struct regs)
+{
+};
+
+static void print_return_value(unsigned long long ret_value, s_syscall_t *temp, size_t option)
+{
+    if (option) {
+        if (temp->retval == T_INTEGER)
+            printf("%lld\n", ret_value);
+        else
+            printf("0x%llx\n", ret_value);
+    } else {
+        if (ret_value)
+            printf("0x%llx\n", ret_value);
+        else
+            printf("0x0\n");
+    }
+}
+
+void print_syscall_ft(pid_t child, struct user_regs_struct regs, size_t option)
+{
+    int syscall_nb = regs.orig_rax;
+    int ret_value = regs.rax;
+    s_syscall_t *temp = NULL;
+
+    temp = get_syscall_by_value(syscall_nb);
+    printf("%s(", temp->name);
+    print_argument_function(regs);
+    printf(") = ");
+    print_return_value(ret_value, temp, option);
 }
--- src/trace.c
+++ src/trace.c
@@ -21,49 +21,6 @@
 #include "../include/strace.h"
 #include "syscalls.c"
 
-/*
-int do_trace(pid_t child)
-{
-    long test = 0;
-    int status = 0;
-
-    waitpid(child, &status, 0);
-    ptrace(PTRACE_SINGLESTEP, child, 0, NULL);
-    test = ptrace(PTRACE_GETREGS, child, 0, sizeof(long)*RAX);
-    return 0;
-}
-*/
-
-int process_running(pid_t child)
-{
-    struct user_regs_struct register;
-    long ret_value = 0;
-
-    ret_value = ptrace(PTRACE_ATTACH, child);
-    if (ret_value == -1)
-        return print_error("Error: attach to running processus failed.\n");
-    return (int)ret_value;
-}
-
-int load_child(int ac, char **av)
-{
-    int var_check = 0;
-    char *args[ac + 1];
-
-    memcpy(args, av, ac * sizeof(char *));
-    args[ac] = NULL;
-    var_check = ptrace(PTRACE_TRACEME);
-    if (var_check == -1)
-        return print_error("Error: ptrace traceme failed.\n");
-    var_check = kill(getpid(), SIGSTOP);
-    if (var_check == -1)
-        return print_error("Error: kill pid with sigstop failed.\n");
-    var_check = execvp(args[2], args);
-    if (var_check == -1)
-        return print_error("Error: excution error.\n");
-    return 0;
-}
-
 static int waitchild(pid_t pid)
 {
     int status;
@@ -79,74 +36,20 @@ static int waitchild(pid_t pid)
     }
 }
 
-static int strace_syscall_get_by_id(unsigned long long id, struct s_syscall *scallp)
-{
-    size_t	i;
-
-    i = 0;
-    while (g_syscalls[i].name != NULL)
-    {
-        if (g_syscalls[i].id == id)
-        {
-            *scallp = g_syscalls[i];
-            return (0);
-        }
-        i++;
-    }
-    return (-1);
-}
-
-/*
-// methode 2
-void syscall_value(int id, my_struct_s temp)
-{
-    int i = 0;
-
-    while (g_syscalls[i].name){
-        if (g_syscalls[i].id == id) {
-            temp.id = g_syscalls[i].id;
-            temp.name = g_syscalls[i].name;
-            temp.argc = g_syscalls[i].argc;
-            temp.noreturn = g_syscalls[i].noreturn;
-            temp.retval = g_syscalls[i].retval;
-            //*temp = temp;
-            printf("id = %lld\n", temp.id);
-            //printf("%d\n", temp.retval);
-            //printf("%d\n", temp.noreturn);
-            printf("argc = %zu\n", temp.argc);
-            printf("name = %s\n", temp.name);
-            return;
-        }
-        printf("toto");
-        i++;
-    }
-}*/
-
-// methode 1
-s_syscall_t *get_syscall_by_value(int id)
-{
-    int i = 0;
-
-    while (g_syscalls[i].name) {
-        if (g_syscalls[i].id == id)
-            return (&g_syscalls[i]);
-        i++;
-    }
-    return NULL;
-}
-
 int strace(pid_t child, size_t option)
 {
     struct user_regs_struct regs;
-    s_syscall_t *temp = NULL;
+    unsigned short int check_syscall;
 
     while (waitchild(child) < 1) {
         ptrace(PTRACE_GETREGS, child, NULL, &regs);
-        temp = get_syscall_by_value(regs.orig_rax);
-        printf("testuuu = %s\n", temp->name);
-        //    ptrace(PTRACE_SINGLESTEP, child);
-        //     FUNCTION QUI PRINT TOUT BIEN AVEC LA STRUCTURE EN PARAMETRE
-        //}
+        check_syscall = ptrace(PTRACE_PEEKTEXT, child, regs.rip);
+        ptrace(PTRACE_SINGLESTEP, child);
+        if (waitchild(child) < 1) {
+            ptrace(PTRACE_GETREGS, child, NULL, &regs);
+            if (check_syscall == 0x80)
+                print_syscall_ft(child, regs, option);
+        }
     }
     return 0;
 }
